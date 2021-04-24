1957c3daeefbc907e69fd9f00ab74cbd88d7a258
이선영 <qrsty9636@gmai.com>
2021-04-24 21:15:34 +0900

    Initial commit

---
+++ src/main/java/com/spring/myspring/sample/controller/SampleController.java
@@ -0,0 +1,11 @@
+package com.spring.myspring.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/sample")
+public class SampleController {
+
+
+}
